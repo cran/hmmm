8711d9b4f856fd36bc528f3343f706b5f6225d11
Colombi Roberto <colombi@unibg.it>
2014-08-14 00:00:00 +0000

    version 1.0-3

--- inst/doc/tutorial.R
+++ inst/doc/tutorial.R
@@ -1,13 +1,13 @@
-### R code from vignette source 'TUTORIAL_hmmm.rnw'
+### R code from vignette source 'tutorial.rnw'
 
 ###################################################
-### code chunk number 1: TUTORIAL_hmmm.rnw:179-180
+### code chunk number 1: tutorial.rnw:181-182
 ###################################################
 options(prompt = "R> ", continue = "+  ", width = 80, useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 2: TUTORIAL_hmmm.rnw:182-185
+### code chunk number 2: tutorial.rnw:184-187
 ###################################################
 library("hmmm")
 data("accident", package = "hmmm")
@@ -15,13 +15,13 @@ accident[1:20,]
 
 
 ###################################################
-### code chunk number 3: TUTORIAL_hmmm.rnw:196-197
+### code chunk number 3: tutorial.rnw:198-199
 ###################################################
 y <- getnames(accident, st = 9)
 
 
 ###################################################
-### code chunk number 4: TUTORIAL_hmmm.rnw:199-202
+### code chunk number 4: tutorial.rnw:201-204
 ###################################################
 count<-cbind(row.names(y)[1:3],y[1:3])
 colnames(count)<-c("cell names", "counts")
@@ -29,14 +29,14 @@ print(count,quote=F)
 
 
 ###################################################
-### code chunk number 5: TUTORIAL_hmmm.rnw:217-219
+### code chunk number 5: tutorial.rnw:219-221
 ###################################################
 margin <- marg.list(c("marg-marg-b-b", "b-marg-b-b", 
 "marg-b-b-b", "b-b-b-b"))
 
 
 ###################################################
-### code chunk number 6: TUTORIAL_hmmm.rnw:224-227
+### code chunk number 6: tutorial.rnw:226-229
 ###################################################
 model <- hmmm.model(marg = margin, lev = c(3, 4, 3, 2),
 names = c("Type", "Time", "Age", "Hour"))
@@ -44,7 +44,7 @@ model
 
 
 ###################################################
-### code chunk number 7: TUTORIAL_hmmm.rnw:241-244
+### code chunk number 7: tutorial.rnw:243-246
 ###################################################
 modelB <- hmmm.model(marg = margin, lev = c(3, 4, 3, 2),
 names = c("Type", "Time", "Age", "Hour"),
@@ -52,26 +52,26 @@ sel = c(12:13, 14:17))
 
 
 ###################################################
-### code chunk number 8: TUTORIAL_hmmm.rnw:248-250
+### code chunk number 8: tutorial.rnw:250-252
 ###################################################
 modB <- hmmm.mlfit(y, modelB)
 modB
 
 
 ###################################################
-### code chunk number 9: TUTORIAL_hmmm.rnw:254-255 (eval = FALSE)
+### code chunk number 9: tutorial.rnw:256-257 (eval = FALSE)
 ###################################################
 ## print(modB, aname = "model B", printflag = TRUE)
 
 
 ###################################################
-### code chunk number 10: TUTORIAL_hmmm.rnw:259-260 (eval = FALSE)
+### code chunk number 10: tutorial.rnw:261-262 (eval = FALSE)
 ###################################################
 ## summary(modB)
 
 
 ###################################################
-### code chunk number 11: TUTORIAL_hmmm.rnw:278-282
+### code chunk number 11: tutorial.rnw:280-284
 ###################################################
 modelA <- hmmm.model(marg = margin, lev = c(3, 4, 3, 2),
 names = c("Type", "Time", "Age", "Hour"), sel = c(12:13, 14:17),
@@ -80,13 +80,13 @@ modA <- hmmm.mlfit(y, modelA)
 
 
 ###################################################
-### code chunk number 12: TUTORIAL_hmmm.rnw:286-287
+### code chunk number 12: tutorial.rnw:288-289
 ###################################################
 anova(modA, modB)
 
 
 ###################################################
-### code chunk number 13: TUTORIAL_hmmm.rnw:294-298 (eval = FALSE)
+### code chunk number 13: tutorial.rnw:296-300 (eval = FALSE)
 ###################################################
 ## modellog <- loglin.model(lev = c(3, 4, 3, 2),
 ## formula = ~ Type * Age * Hour + Time * Age * Hour + Type : Time,
@@ -95,14 +95,14 @@ anova(modA, modB)
 
 
 ###################################################
-### code chunk number 14: TUTORIAL_hmmm.rnw:333-335
+### code chunk number 14: tutorial.rnw:335-337
 ###################################################
 margin <- marg.list(c("marg-marg-l-l", "g-marg-l-l", 
 "marg-g-l-l", "g-g-l-l"))
 
 
 ###################################################
-### code chunk number 15: TUTORIAL_hmmm.rnw:346-349
+### code chunk number 15: tutorial.rnw:348-351
 ###################################################
 model <- hmmm.model(marg = margin, lev = c(3, 3, 2, 4), 
 names = c("In", "Sa", "Co", "Ho"))
@@ -110,7 +110,7 @@ model
 
 
 ###################################################
-### code chunk number 16: TUTORIAL_hmmm.rnw:356-362
+### code chunk number 16: tutorial.rnw:358-364
 ###################################################
 model1 <- hmmm.model(marg = margin, lev = c(3, 3, 2, 4),
 names = c("In", "Sa", "Co", "Ho"), sel = c(18:23, 26:27, 34:39))
@@ -121,7 +121,7 @@ mod1
 
 
 ###################################################
-### code chunk number 17: TUTORIAL_hmmm.rnw:366-370
+### code chunk number 17: tutorial.rnw:368-372
 ###################################################
 model2 <- hmmm.model(marg = margin, lev = c(3, 3, 2, 4),
 names = c("In", "Sa", "Co", "Ho"), sel = c(18:23, 34:39))
@@ -130,7 +130,7 @@ mod2
 
 
 ###################################################
-### code chunk number 18: TUTORIAL_hmmm.rnw:379-383
+### code chunk number 18: tutorial.rnw:381-385
 ###################################################
 model3 <- hmmm.model(marg = margin, lev = c(3, 3, 2, 4),
 names = c("In", "Sa", "Co", "Ho"), sel = c(18:23, 34:39, 44:71))
@@ -139,37 +139,37 @@ mod3
 
 
 ###################################################
-### code chunk number 19: TUTORIAL_hmmm.rnw:427-428
+### code chunk number 19: tutorial.rnw:429-430
 ###################################################
 marginals <- marg.list(c("r-marg", "marg-r", "r-r"))
 
 
 ###################################################
-### code chunk number 20: TUTORIAL_hmmm.rnw:441-443
+### code chunk number 20: tutorial.rnw:443-445
 ###################################################
 rec1 <- matrix(c(-1, -1,  1,
-               -1,  1,  0), 2, 3, byrow = TRUE)
+                 -1,  1,  0), 2, 3, byrow = TRUE)
 
 
 ###################################################
-### code chunk number 21: TUTORIAL_hmmm.rnw:452-458
+### code chunk number 21: tutorial.rnw:454-460
 ###################################################
-rec2<-matrix(c(-1, -1, -1,  0,  1,  1,  1,
-             -1, -1, -1,  1,  0,  0,  0,
-              1, -1,  0,  0,  0,  0,  0,
-              0, -1,  1,  0,  0,  0,  0,
-              0,  0,  0,  0,  1, -1,  0,
-              0,  0,  0,  0,  0, -1,  1), 6, 7, byrow = TRUE)
+rec2 <- matrix(c(-1, -1, -1,  0,  1,  1,  1,
+                 -1, -1, -1,  1,  0,  0,  0,
+                  1, -1,  0,  0,  0,  0,  0,
+                  0, -1,  1,  0,  0,  0,  0,
+                  0,  0,  0,  0,  1, -1,  0,
+                  0,  0,  0,  0,  0, -1,  1), 6, 7, byrow = TRUE)
 
 
 ###################################################
-### code chunk number 22: TUTORIAL_hmmm.rnw:466-467
+### code chunk number 22: tutorial.rnw:468-469
 ###################################################
 rec <- recursive(rec1, rec2)
 
 
 ###################################################
-### code chunk number 23: TUTORIAL_hmmm.rnw:475-478
+### code chunk number 23: tutorial.rnw:477-480
 ###################################################
 model <- hmmm.model(marg = marginals, lev = c(3, 7), 
 names = c("Rel", "Pol"), cocacontr = rec)
@@ -177,7 +177,7 @@ model
 
 
 ###################################################
-### code chunk number 24: TUTORIAL_hmmm.rnw:511-515
+### code chunk number 24: tutorial.rnw:513-517
 ###################################################
 Emat <- cbind(matrix(0, 2, 4), matrix(c(1, 0, 0, 1, 0, -1, -1, 0), 2, 4), 
 matrix(0, 2, 12))
@@ -186,7 +186,7 @@ names = c("Rel", "Pol"), cocacontr = rec, E = Emat)
 
 
 ###################################################
-### code chunk number 25: TUTORIAL_hmmm.rnw:520-524
+### code chunk number 25: tutorial.rnw:522-526
 ###################################################
 data("relpol", package = "hmmm")
 y <- getnames(relpol, st = 4)
@@ -195,14 +195,14 @@ print(modE)
 
 
 ###################################################
-### code chunk number 26: TUTORIAL_hmmm.rnw:540-542
+### code chunk number 26: tutorial.rnw:542-544
 ###################################################
 data("depression", package="hmmm")
 y <- getnames(depression, st = 9)
 
 
 ###################################################
-### code chunk number 27: TUTORIAL_hmmm.rnw:566-571
+### code chunk number 27: tutorial.rnw:568-573
 ###################################################
 margin <- marg.list(c("marg-marg-marg-b-b","b-marg-marg-b-b",
 "marg-b-marg-b-b", "marg-marg-b-b-b","b-b-b-b-b"))
@@ -212,7 +212,7 @@ modelsat
 
 
 ###################################################
-### code chunk number 28: TUTORIAL_hmmm.rnw:589-601
+### code chunk number 28: tutorial.rnw:591-603
 ###################################################
 A1<-matrix(c(
    0,0,0,1,0,0,0,0,0,0,0,0,
@@ -229,7 +229,7 @@ E1<-cbind(matrix(0,8,3), A1, matrix(0,8,16))
 
 
 ###################################################
-### code chunk number 29: TUTORIAL_hmmm.rnw:606-610
+### code chunk number 29: tutorial.rnw:608-612
 ###################################################
 model1<-hmmm.model(marg = margin, lev =c(2,2,2,2,2), names = name, E = E1)
 
@@ -238,7 +238,7 @@ fitmod1
 
 
 ###################################################
-### code chunk number 30: TUTORIAL_hmmm.rnw:615-626
+### code chunk number 30: tutorial.rnw:617-628
 ###################################################
 A2<-matrix(c(
     0,0,0,1,0,0,0,0,0,0,0,0,
@@ -254,7 +254,7 @@ E2<-cbind(matrix(0,7,3), A2, matrix(0,7,16))
 
 
 ###################################################
-### code chunk number 31: TUTORIAL_hmmm.rnw:631-635
+### code chunk number 31: tutorial.rnw:633-637
 ###################################################
 model2<-hmmm.model(marg = margin, lev = c(2,2,2,2,2), names = name, E = E2)
 
@@ -263,7 +263,7 @@ fitmod2
 
 
 ###################################################
-### code chunk number 32: TUTORIAL_hmmm.rnw:645-650
+### code chunk number 32: tutorial.rnw:647-652
 ###################################################
 model3<-hmmm.model(marg = margin, lev = c(2,2,2,2,2), names = name, E = E2,
                    formula=~R1*R2*T*D+R3*R2*T*D )
@@ -273,13 +273,13 @@ fitmod3
 
 
 ###################################################
-### code chunk number 33: TUTORIAL_hmmm.rnw:676-677
+### code chunk number 33: tutorial.rnw:678-679
 ###################################################
 marginals <- marg.list(c("b-marg", "marg-g", "b-g"))
 
 
 ###################################################
-### code chunk number 34: TUTORIAL_hmmm.rnw:691-696
+### code chunk number 34: tutorial.rnw:693-698
 ###################################################
 al <- list(
 Type = ~ Type * (Age + Hour),
@@ -289,7 +289,7 @@ Type.Time = ~ Type.Time * (Age + Hour)
 
 
 ###################################################
-### code chunk number 35: TUTORIAL_hmmm.rnw:711-714
+### code chunk number 35: tutorial.rnw:713-716
 ###################################################
 model <- hmmm.model.X(marg = marginals, lev = c(3, 4), 
 names = c("Type", "Time"), Formula = al, strata = c(3, 2), 
@@ -297,7 +297,7 @@ fnames = c("Age", "Hour"))
 
 
 ###################################################
-### code chunk number 36: TUTORIAL_hmmm.rnw:720-724
+### code chunk number 36: tutorial.rnw:722-726
 ###################################################
 data("accident", package = "hmmm")
 y <- getnames(accident, st = 9)
@@ -306,13 +306,13 @@ mod1
 
 
 ###################################################
-### code chunk number 37: TUTORIAL_hmmm.rnw:730-731 (eval = FALSE)
+### code chunk number 37: tutorial.rnw:732-733 (eval = FALSE)
 ###################################################
 ## summary(mod1)
 
 
 ###################################################
-### code chunk number 38: TUTORIAL_hmmm.rnw:743-748
+### code chunk number 38: tutorial.rnw:745-750
 ###################################################
 alind <- list(
 Type = ~ Type * Age + Type * Hour,
@@ -322,7 +322,7 @@ Type.Time = "zero"
 
 
 ###################################################
-### code chunk number 39: TUTORIAL_hmmm.rnw:760-765
+### code chunk number 39: tutorial.rnw:762-767
 ###################################################
 alpar <- list(
 Type = ~ Type + Age + Hour,
@@ -332,7 +332,7 @@ Type.Time = ~ Type.Time + Age + Hour
 
 
 ###################################################
-### code chunk number 40: TUTORIAL_hmmm.rnw:798-802
+### code chunk number 40: tutorial.rnw:800-804
 ###################################################
 data("polbirth", package = "hmmm")
 y <- getnames(polbirth)
@@ -341,32 +341,32 @@ names <- c("Politics", "Birth")
 
 
 ###################################################
-### code chunk number 41: TUTORIAL_hmmm.rnw:808-809
+### code chunk number 41: tutorial.rnw:810-811
 ###################################################
 ineq <- list(marg = c(1, 2), int = list(c(1, 2)), types = c("g", "l"))
 
 
 ###################################################
-### code chunk number 42: TUTORIAL_hmmm.rnw:815-817
+### code chunk number 42: tutorial.rnw:817-819
 ###################################################
 model <- hmmm.model(marg = marginals, dismarg = ineq, lev = c(7, 4), 
 names = names)
 
 
 ###################################################
-### code chunk number 43: TUTORIAL_hmmm.rnw:827-828
+### code chunk number 43: tutorial.rnw:829-830
 ###################################################
 mlr <- hmmm.mlfit(y, model, noineq = FALSE)
 
 
 ###################################################
-### code chunk number 44: TUTORIAL_hmmm.rnw:834-835
+### code chunk number 44: tutorial.rnw:836-837
 ###################################################
 msat <- hmmm.mlfit(y, model)
 
 
 ###################################################
-### code chunk number 45: TUTORIAL_hmmm.rnw:841-844
+### code chunk number 45: tutorial.rnw:843-846
 ###################################################
 model0 <- hmmm.model(marg = marginals, lev = c(7, 4), sel = c(10:27), 
 names = names)
@@ -374,32 +374,32 @@ mnull <- hmmm.mlfit(y, model0)
 
 
 ###################################################
-### code chunk number 46: TUTORIAL_hmmm.rnw:854-855
+### code chunk number 46: tutorial.rnw:856-857
 ###################################################
 test <- hmmm.chibar(nullfit = mnull, disfit = mlr, satfit = msat)
 
 
 ###################################################
-### code chunk number 47: TUTORIAL_hmmm.rnw:874-875
+### code chunk number 47: tutorial.rnw:876-877
 ###################################################
 test
 
 
 ###################################################
-### code chunk number 48: TUTORIAL_hmmm.rnw:896-897
+### code chunk number 48: tutorial.rnw:898-899
 ###################################################
 y <- matrix(c(104, 24, 65, 76, 146, 30, 50, 9, 166), 9, 1)
 
 
 ###################################################
-### code chunk number 49: TUTORIAL_hmmm.rnw:908-910
+### code chunk number 49: tutorial.rnw:910-912
 ###################################################
 Zmat <- kronecker(diag(3), matrix(1, 3, 1))
 ZFmat <- kronecker(diag(3), matrix(1, 3, 1))[,3]
 
 
 ###################################################
-### code chunk number 50: TUTORIAL_hmmm.rnw:922-929
+### code chunk number 50: tutorial.rnw:924-931
 ###################################################
 Gini <- function(m) {
 A<-matrix(m,3,3,byrow=TRUE)
@@ -411,19 +411,19 @@ A<-matrix(m,3,3,byrow=TRUE)
 
 
 ###################################################
-### code chunk number 51: TUTORIAL_hmmm.rnw:939-940
+### code chunk number 51: tutorial.rnw:941-942
 ###################################################
 mod_eq <- mphineq.fit(y, Z = Zmat, ZF = ZFmat, h.fct = Gini)
 
 
 ###################################################
-### code chunk number 52: TUTORIAL_hmmm.rnw:948-949
+### code chunk number 52: tutorial.rnw:951-952
 ###################################################
 mod_ineq <- mphineq.fit(y, Z = Zmat, ZF = ZFmat, d.fct = Gini)
 
 
 ###################################################
-### code chunk number 53: TUTORIAL_hmmm.rnw:955-957
+### code chunk number 53: tutorial.rnw:958-960
 ###################################################
 mod_sat <- mphineq.fit(y, Z = Zmat, ZF = ZFmat)
 hmmm.chibar(nullfit = mod_eq, disfit = mod_ineq, satfit = mod_sat)
